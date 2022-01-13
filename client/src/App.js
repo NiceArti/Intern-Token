63ced6ad08ddfd65c943438921ca807e730d515a
Cravcenco Nichita <nice_arti@mail.ru>
2022-01-13 15:34:29 +0200

    first commit

--- client/src/App.js
+++ client/src/App.js
@@ -9,10 +9,10 @@ class App extends Component {
     web3: null,
     accounts: null,
     contract: null, 
-    balanceOfMsgSender: null,
+    balanceOfMsgSender: 0,
     anotherAccount: null,
     amount: 0,
-    whitelisted: false
+    //isWhitelisted: false
   };
 
   componentDidMount = async () => 
@@ -23,6 +23,7 @@ class App extends Component {
       this.transfer = this.transfer.bind(this);
       this.burn = this.burn.bind(this);
       this.mint = this.mint.bind(this);
+      this.addToWhitelist = this.addToWhitelist.bind(this);
 
 
       // Get network provider and web3 instance.
@@ -39,10 +40,9 @@ class App extends Component {
         deployedNetwork && deployedNetwork.address,
       );
       const balanceOfMsgSender = await instance.methods.balanceOf(accounts[0]).call();
-      const whitelisted = await instance.methods.whitelisted.call(accounts[0]);
+      //const isWhitelisted = await instance.methods.whitelisted().call(accounts[0]);
 
       console.log(instance);
-      console.log(whitelisted);
       
 
       // Set web3, accounts, and contract to the state, and then proceed with an
@@ -52,7 +52,7 @@ class App extends Component {
         accounts,
         contract: instance,
         balanceOfMsgSender,
-        whitelisted
+        //isWhitelisted
       });
     } 
     catch (error) 
@@ -70,14 +70,10 @@ class App extends Component {
     var mint = document.getElementById("mint");
     var mintVal = document.getElementById("mintVal");
 
-    if(mintVal.value.length != 0)
-    {
-        mint.disabled = false;
-    }
+    if(mintVal.value.length !== 0)
+      mint.disabled = false;
     else
-    {
-        mint.disabled = true;
-    }
+      mint.disabled = true;
   }
 
 
@@ -86,7 +82,7 @@ class App extends Component {
     var burn = document.getElementById("burn");
     var burnVal = document.getElementById("burnVal");
 
-    if(burnVal.value.length != 0)
+    if(burnVal.value.length !== 0)
         burn.disabled = false;
     else 
         burn.disabled = true;
@@ -112,6 +108,13 @@ class App extends Component {
     const {amount, contract, accounts} = this.state;
     await contract.methods.mint(amount).send({from: accounts[0]});
   }
+//!=
+  async addToWhitelist(e)
+  {
+    e.preventDefault();
+    const {anotherAccount, contract, accounts} = this.state;
+    await contract.methods.addToWhitelist(anotherAccount).send({from: accounts[0]});
+  }
 
 
   to(e)
@@ -132,8 +135,7 @@ class App extends Component {
       <div class="container">
         <div id="header">
           <h1 align="center">Intern Token</h1>
-          <span id="balance" >Balance: {this.state.balanceOfMsgSender}</span>
-          <span id="balance" >Balance: {this.state.whitelisted}</span>
+          <span id="balance" >Balance: {this.state.balanceOfMsgSender} INT</span>
           <span id="account" >Account: {this.state.accounts}</span>
         </div>
         <hr width="99%"/>
@@ -153,6 +155,66 @@ class App extends Component {
           <input type="submit" id="burn" disabled="true" value="Burn"/>
           <input type="text" placeholder="10" id="burnVal" class="amount" onChange={this.amount} onKeyUp={this.enableBurn}/>
         </form>
+
+        <form name="WhiteListForm" onSubmit={this.addToWhitelist}>
+          <input type="submit" id="addToWhitelist" value="Add in whitelist"/>
+          <input type="text" placeholder="0x38HG4NO..." id="address" onChange={this.to}/>
+        </form>
+
+        <br/><br/><br/><br/><br/><br/>
+        <div id="content" align="center">
+          <table>
+            <thead>
+              <tr>
+                <th>Telegram</th>
+                <th>Name</th>
+                <th>Address</th>
+              </tr>
+            </thead>
+            <tbody> 
+              <tr>
+                <th>@flexxxbob</th>
+                <th>Alexander</th>
+                <th>0xF0802b38060acF11823d1d3870f0775F454e14B2</th>
+              </tr>
+              <tr>
+                <th>@Valdemar1337</th>
+                <th>Vladimir</th>
+                <th>...</th>
+              </tr>
+              <tr>
+                <th>@Nektarines</th>
+                <th>Nichita</th>
+                <th>...</th>
+              </tr>
+              <tr>
+                <th>@zhaukenove</th>
+                <th>Yerkebulan</th>
+                <th>...</th>
+              </tr>
+              <tr>
+                <th>@Igorterzi</th>
+                <th>Igor</th>
+                <th>...</th>
+              </tr>
+              <tr>
+                <th>@vadim_peev</th>
+                <th>Vadim</th>
+                <th>...</th>
+              </tr>
+              <tr>
+                <th>@sweetbubalehj</th>
+                <th>Egor</th>
+                <th>...</th>
+              </tr>
+              <tr>
+                <th>@Kcintes</th>
+                <th>Nicolai</th>
+                <th>...</th>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     );
   }
